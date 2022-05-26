131a096e6534f1ab2205f8ef9d8b4404c6b02d04
Davide Frison <davide.frison@zextras.com>
2022-05-26 12:14:06 +0200

    chore(build-ivysettings.xml): - maven central repo resolver move up - workaround to stop resolving jar recursively chore (client-ivy.xml): junit 5 refactor:  client tests to use junit 5

--- client/src/java-test/com/zimbra/client/ZDocumentTest.java
+++ client/src/java-test/com/zimbra/client/ZDocumentTest.java
@@ -5,12 +5,11 @@
 
 package com.zimbra.client;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.common.base.Strings;
-import com.zimbra.client.ZDocument;
 import com.zimbra.common.soap.Element;
+import org.junit.jupiter.api.Test;
 
 public class ZDocumentTest {
 
@@ -20,12 +19,12 @@ public class ZDocumentTest {
             "<doc f='' d='1300925565000' rev='2' ms='2' l='0-0-0:16' ver='1' ct='text/plain' id='0-0-0:257' cr='' " +
             "loid='' t='' s='18' md='1300925565000' leb='' name='doc.txt' descEnabled='1' cd='1300925565000'><meta/><fr>This is a document</fr></doc>";
         ZDocument doc = new ZDocument(Element.parseXML(xml));
-        Assert.assertEquals(null, Strings.emptyToNull(doc.getFlags()));
-        
+        assertEquals(null, Strings.emptyToNull(doc.getFlags()));
+
         xml =
             "<doc f='t' d='1300925565000' rev='3' ms='4' l='0-0-0:16' ver='1' ct='text/plain' id='0-0-0:258' cr='' " +
             "loid='' t='' s='14' md='1300925565000' leb='' name='note.txt' descEnabled='1' cd='1300925565000'><meta/><fr>This is a note</fr></doc>";
         ZDocument note = new ZDocument(Element.parseXML(xml));
-        Assert.assertEquals("t", note.getFlags());
+        assertEquals("t", note.getFlags());
     }
 }
--- client/src/java-test/com/zimbra/client/ZTagTest.java
+++ client/src/java-test/com/zimbra/client/ZTagTest.java
@@ -5,9 +5,10 @@
 
 package com.zimbra.client;
 
-import junit.framework.Assert;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 
 public class ZTagTest {
@@ -16,10 +17,10 @@ public class ZTagTest {
     public void testColor() throws Exception {
         // 4451821 is equivalent long value for cyan
         ZTag.Color color = ZTag.Color.fromString("4451821");
-        Assert.assertEquals(color.name(), "cyan");
+        assertEquals(color.name(), "cyan");
         color = ZTag.Color.fromString("blue");
-        Assert.assertEquals(color.name(), "blue");
+        assertEquals(color.name(), "blue");
         color = ZTag.Color.fromString("0x5b9bf2");
-        Assert.assertEquals(color.name(), "orange");
+        assertEquals(color.name(), "orange");
     }
 }
